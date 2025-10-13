d28ecebba92b6beb4a09aa642e6f5d88408b26a5
Joel Sánchez Simón <jsa5214@office.proven.cat>
2025-10-13 18:05:20 +0200

    class end

--- exercicis/pt1-3/fitxers_terraform/main.tf
+++ exercicis/pt1-3/fitxers_terraform/main.tf
@@ -10,6 +10,13 @@ resource "aws_instance" "ex-1" {
     count = 2 # Creates 2 EC2 instances
 
     tags = {
-      Name = "Exercici 1 Pt1.3"
+      Name = "Exercici 1 - ${count.index + 1}" # Adds a number to each "same" instance
     }
-}  
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16" # VPC IP Network
+  tags = {
+    Name = "VPC-Practica" # VPC Name
+  }
+}
