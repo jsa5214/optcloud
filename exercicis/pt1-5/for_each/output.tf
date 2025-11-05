bb9d16914f644a82a1b656775e538e9c71c828b4
Joel Sánchez Simón <jsa5214@office.proven.cat>
2025-11-05 01:19:13 +0100

    output.tf duly commented

--- exercicis/pt1-5/for_each/output.tf
+++ exercicis/pt1-5/for_each/output.tf
@@ -1,42 +1,54 @@
-# Any resource declared with count becomes a list of instances, so you must reference them using []. 
-# ex(count): aws_resource_name.terraform_name[index].attribute
-# ex(for_each): aws_resource_name.terraform_name["key"].attribute
+# Any resource declared with for_each becomes a MAP of instances, so you must reference them using ["key"].attribute
+# ex(for_each): aws_resource_name.terraform_name["public-us-east-1a-1"].id
 
 # ---------- OUTPUTS EC2 ----------
+# Maps preserve the instance keys you defined with for_each (e.g., "public-us-east-1a-1")
+# This makes it easy to correlate attributes back to their instances.
+
 output "public_instance_ids" {
-  description = "IDs of public EC2 instances"
-  #  inst_key, inst pair example // inst_key => instance object
-  #  "public-us-east-1a-1" = { id = "i-0123456789abcdef0", public_ip = "54.45.12.34", private_ip = "10.0.1.10" }
+  description = "EC2 public instances IDs keyed by instance key"
   value = {
     for inst_key, inst in aws_instance.ec2_public :
     inst_key => inst.id
   }
 }
 
-
 output "public_instance_public_ips" {
-  description = "EC2 public instances public IP's"
-  value       = aws_instance.ec2_public[*].public_ip
+  description = "EC2 public instances public IPs keyed by instance key"
+  value = {
+    for inst_key, inst in aws_instance.ec2_public :
+    inst_key => inst.public_ip
+  }
 }
 
 output "public_instance_private_ips" {
-  description = "EC2 public instances private IP's"
-  value       = aws_instance.ec2_public[*].private_ip
+  description = "EC2 public instances private IPs keyed by instance key"
+  value = {
+    for inst_key, inst in aws_instance.ec2_public :
+    inst_key => inst.private_ip
+  }
 }
 
-
 output "private_instance_ids" {
-  description = "EC2 private instances ID's"
-  value       = aws_instance.ec2_private[*].id
+  description = "EC2 private instances IDs keyed by instance key"
+  value = {
+    for inst_key, inst in aws_instance.ec2_private :
+    inst_key => inst.id
+  }
 }
 
 output "private_instance_private_ips" {
-  description = "EC2 private instances private IP's"
-  value       = aws_instance.ec2_private[*].private_ip
+  description = "EC2 private instances private IPs keyed by instance key"
+  value = {
+    for inst_key, inst in aws_instance.ec2_private :
+    inst_key => inst.private_ip
+  }
 }
 
 # ---------- OUTPUT S3 ----------
+# Use of try() avoids errors when the bucket doesn't exist.
+# If not created, expose null.
 output "s3_bucket_name" {
-  description = "Bucket Name"
-  value       = var.create_s3_bucket ? aws_s3_bucket.s3_bucket[0].bucket : "S3 bucket creation is set to false"
+  description = "Bucket name if created, otherwise null"
+  value       = try(aws_s3_bucket.s3_bucket[0].bucket, null)
 }
